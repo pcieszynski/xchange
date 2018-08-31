72ef98ee41e89dfccb55b0b21938b0dae767dc4a
pcieszynski <p_cieszynski@onet.pl>
2018-08-31 18:37:23 +0200

    adding project

---
+++ xchange/src/main/java/com/ender/xchange/xchange/XchangeApplication.java
@@ -0,0 +1,12 @@
+package com.ender.xchange.xchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XchangeApplication.class, args);
+	}
+}
